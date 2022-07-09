256fc06c25fee1517c29ac0d6bc5d586e4c6d135
Daw-Ran Liou <hi@dawranliou.com>
2022-07-09 08:31:20 -0700

    Bind C-o to occur in isearch mode

--- init.el
+++ init.el
@@ -147,6 +147,8 @@ Inspired by https://github.com/katspaugh/ido-at-point"
 (keymap-global-set "C-c r" 'rg)
 (keymap-global-set "C-;" 'iedit-mode)
 (keymap-global-set "C-c g" 'grep-find)
+(with-eval-after-load 'isearch
+  (keymap-set isearch-mode-map "C-o" 'isearch-occur))
 (global-set-key [remap kill-region] 'backward-kill-word-or-region)
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 
