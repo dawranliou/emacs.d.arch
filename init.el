9ac392b5c4355a63852a505662ec6a3267b591f8
Daw-Ran Liou <hi@dawranliou.com>
2022-09-13 11:51:01 +0800

    Fix fixed-pitch-serif font

--- init.el
+++ init.el
@@ -84,7 +84,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch-serif ((t (:height 1.4 :family "Monospace Serif")))))
 
 (put 'narrow-to-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
