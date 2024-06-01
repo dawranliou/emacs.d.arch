6e25439ceddfb325f5770012820d20e454921646
Daw-Ran Liou <hi@dawranliou.com>
2024-06-01 09:54:43 -0700

    Bind C-c c to compile

--- init.el
+++ init.el
@@ -194,6 +194,7 @@ backwards."
 (keymap-global-set "M-i" 'delete-other-windows)
 (keymap-global-set "M-j" #'avy-goto-char-timer) ; was #'default-indent-new-line
 (keymap-global-set "C-c j" #'avy-goto-line)
+(keymap-global-set "C-c c" #'compile)
 (keymap-global-set "M-z" 'zap-up-to-char)
 (keymap-global-set "M-Z" 'zap-to-char)
 (keymap-global-set "M-$" #'jinx-correct)
