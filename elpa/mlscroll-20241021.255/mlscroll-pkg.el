0c1ccb0b7f9aed3dfc23178cb91483af66c2a591
Daw-Ran Liou <hi@dawranliou.com>
2024-11-28 21:35:20 -0800

    Sync packages 2024-11-28

---
+++ elpa/mlscroll-20241021.255/mlscroll-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "mlscroll" "20241021.255"
+  "A scroll bar for the modeline."
+  '((emacs "27.1"))
+  :url "https://github.com/jdtsmith/mlscroll"
+  :commit "c1bc37709c27d005190d7fcd6a17376b9413381f"
+  :revdesc "c1bc37709c27"
+  :keywords '("convenience"))
